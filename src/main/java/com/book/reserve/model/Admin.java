fe4a8f938ded64c237c0b312e967becc63547794
JunLyul Yang <sjn3649@gmail.com>
2023-03-27 20:58:37 +0900

    set naver code convention

--- src/main/java/com/book/reserve/model/Admin.java
+++ src/main/java/com/book/reserve/model/Admin.java
@@ -2,31 +2,20 @@ package com.book.reserve.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
-@Table(name = "tb_user")
+@Table(name = "tb_admin")
 @Entity
-public class User extends CommonBaseModel {
+public class Admin extends CommonBaseModel {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
 	@Column(name = "login_id")
-	private String loginId;
-
-	@JoinColumn(name = "library_id")
-	@ManyToOne(targetEntity = Library.class, fetch = FetchType.LAZY)
-	private Library library;
-
-	private String name;
-
-	private String email;
+	private String loginID;
 
 	@Column(name = "enc_password")
 	private String encPassword;
